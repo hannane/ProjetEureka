8fc5e0718da27a732b51010e2da0a2412c778522
killmrak <pichuzhkin.ruslan@mail.com>
2015-03-09 20:48:36 +0100

    update_functions

--- functions.php
+++ functions.php
@@ -37,7 +37,8 @@ function addUser($surname,$name,$country,$mail,$login,$password,$campFr,$isProf,
 	mysql_close();//fermer la BDD
 	return true;
 }
-function isResp($link,$idUser){
+function isResp($idUser){
+$lien=connectDB();
 $query='SELECT isResp FROM users WHERE idUser ="'.$idUser.'"';
 $res=mysql_query($query)or die("Connection ERROR".mysql_error());
 mysql_close();
@@ -47,7 +48,8 @@ return true;
 return false;
 }
 
-function isProf($link,$idUser){
+function isProf($idUser){
+$lien=connectDB();
 $query='SELECT isProf FROM users WHERE idUser ="'.$idUser.'"';
 $res=mysql_query($query)or die("Connection ERROR".mysql_error());
 mysql_close();
@@ -64,7 +66,34 @@ mysql_close();
 return $res;
 }
 
-
-
+function getPwd ($idUser){
+$lien=connectDB();
+$query='SELECT `password` FROM `users` WHERE `idUser` ="'.$idUser.'"';
+$res=mysql_query($query)or die("Connection ERROR".mysql_error());
+mysql_close();
+return $res;
+}
+function getId ($login){
+$lien=connectDB();
+$query='SELECT `idUser` FROM `users` WHERE `login` ="'.$login.'"';
+$res=mysql_query($query)or die("Connection ERROR".mysql_error());
+mysql_close();
+return $res;
+}
+function listEtu(){
+$lien=connectDB();
+$query='SELECT `idUser`,`login` FROM `users` WHERE `isProf` =0 and `isResp`=0';
+$res=mysql_query($query)or die("Connection ERROR".mysql_error());
+echo "<table border=1>\n";
+while(row=mysql_fetch_row($res)){
+$id=$row[0];
+$login=$row[1];
+echo "<tr><td>$id</td>";
+echo "<tr><td>$login</td>";
+echo "</tr>\n";
+}
+echo "</table>\n";
+mysql_close();
+}
 
 ?>
