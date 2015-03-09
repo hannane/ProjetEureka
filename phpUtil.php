ae81fa16380229fe6de17a816ce1344661b4b646
Darkxell <darkxell.mc@gmail.com>
2015-03-09 23:24:39 +0100

    Almost useless
    J'ajoute la fonction tout a l'heure. La je mange ^^

--- phpUtil.php
+++ phpUtil.php
@@ -3,7 +3,12 @@
    Liste de toutes les méthodes */
    
 /*  dataListCountry
- void -> String */
+ void -> String 
+ génère une datalist en String.
+ Documentation sur les datalists : http://41mag.fr/liste-des-balises-html5/balise-datalist-html5
+ ^^^^^^ J'aime bien ce site ya plein de trucs intéressants.
+ Je la code ce soir pas la peine de s'y mettre.
+ */
  
  /*isemail
   String -> vrai ou faux*/ 
