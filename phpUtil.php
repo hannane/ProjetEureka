7e2e9d7dd1b2f9e2f32c3a8b319681ae2b487bce
Darkxell <darkxell.mc@gmail.com>
2015-03-09 23:50:54 +0100

    DataListCountry Added
    Bon sa sert encore a rien, mais sa marche, on peut la mettre dans les pages php comme sa. Il faudra ajouter les pays évidemment.

--- phpUtil.php
+++ phpUtil.php
@@ -7,8 +7,27 @@
  génère une datalist en String.
  Documentation sur les datalists : http://41mag.fr/liste-des-balises-html5/balise-datalist-html5
  ^^^^^^ J'aime bien ce site ya plein de trucs intéressants.
- Je la code ce soir pas la peine de s'y mettre.
  */
+ function dataListCountry(){
+//Bon je commente pas ce que sa fait, le code est plutot simple... Mais comme sa on a pas a le recopier a cheque document HTML.
+//Pour rappel, dans un form on appelle la liste comme sa : <input type="list" list="pays" value="indéfini" name="country" id="country" placeholder="Pays">
+//début du String
+$string = "
+<datalist id='pays'>
+	<option value='indéfini'>
+	<option value='pays2'>
+	<option value='pays3'> 
+	<option value='pays4'>
+	<option value='pays5'>
+
+	
+	
+</datalist>
+" ;// Fin du String
+return $string;
+}
+ 
+ 
  
  /*isemail
   String -> vrai ou faux*/ 
